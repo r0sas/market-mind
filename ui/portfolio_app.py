b3d615059fca3b88887750fd5577b58a3bc25bec
r0sas <rosas.miguel@outlook.pt>
2025-10-17 19:49:12 +0100

    Removed redundant code

--- ui/portfolio_app.py
+++ ui/portfolio_app.py
@@ -1,47 +1,42 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-from core.stock_portfolio import Stock_portfolio
-import plotly.graph_objects as go
+from core.portfolio_manager import PortfolioManager
 # Import UI components
 from ui.components import AddStockForm, PortfolioTable, SummarySection, ChartSection, PlotSelectionDropdown
 
 class PortfolioApp:
     def __init__(self):
         self.selected_stocks = []
+        self.portfolio_manager = PortfolioManager()
 
         st.set_page_config(page_title="Stock ROI Tracker", layout="wide")
         st.title("📈 Stock ROI Tracker")
 
         if "portfolio" not in st.session_state:
             st.session_state.portfolio = pd.DataFrame(columns=[
-                "Stock Symbol", "Shares", "Price", "Acquisition Date", "Current Price"
+                "Ticker", "Shares", "Price", "Date", "Current Price"
             ])
 
     def run(self):
-        AddStockForm().render()
+        AddStockForm(self.portfolio_manager).render()
         df = PortfolioTable().render()
         if df is None:
             return
 
-        selected = SummarySection().render(df)
+        SummarySection().render(df)
 
         if st.button("🗑️ Clear All Data"):
             st.session_state.portfolio = pd.DataFrame(columns=[
-                "Stock Symbol", "Shares", "Price", "Acquisition Date", "Current Price"
+                "Ticker", "Shares", "Price", "Date", "Current Price"
             ])
             st.success("Portfolio cleared.")
-        #PlotSelectionDropdown().render()
-        ChartSection().render(selected)
-        portfolio_rentability_data, stocks_rentability = Stock_portfolio().get_portfolio_historical_rentability(df)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=portfolio_rentability_data["Date"], y=portfolio_rentability_data["Current Value"], mode='lines+markers', name='Current Valuation'))
-        fig.add_trace(go.Scatter(x=portfolio_rentability_data["Date"], y=portfolio_rentability_data["Total Invested"], mode='lines+markers', name='Invested'))
-
-        fig.update_layout(title="Portfolio", xaxis_title="Date", yaxis_title="Value (€)")
-
-        # Display in Streamlit
-        st.plotly_chart(fig, use_container_width=True)
-        
-        fig = px.line(stocks_rentability, x="Date", y="ROI", color="Ticker", title="Stock ROI")
-        st.plotly_chart(fig, use_container_width=True)
+        # PlotSelectionDropdown().render()
+        # portfolio_rentability_data, stocks_history = Stock_portfolio().get_portfolio_historical_rentability(df)
+        stocks_history = self.portfolio_manager.get_portfolio_history(df)
+        print(stocks_history)
+        portfolio_rentability_data = self.portfolio_manager.get_portfolio_rentability(stocks_history)
+        print("Portfolio Rentability")
+        print(portfolio_rentability_data)
+        stocks_rentability = self.portfolio_manager.get_stocks_rentability(stocks_history)
+        final_df = pd.concat([stocks_rentability, portfolio_rentability_data])
+        ChartSection(self.portfolio_manager).render(final_df["Ticker"].unique().tolist(), final_df)
